4779ff07a58313481d962b0ed95d0fcf03a0f8cc
Ryu <nguyenductruong200604@gmail.com>
2026-03-03 16:03:19 +0700

    Fix thêm room-chat

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -13,47 +13,49 @@ export const postRoomChat = async (req, res) => {
             return res.status(400).json({ message: "Không thể chat với bản thân" })
         }
         let hash = [userId, receiverId].sort().join("_");
-        try {
-            const receiver = await User.findById(receiverId).select("displayName");
-            const sender = await User.findById(userId).select("displayName");
-            const room = await RoomChat.create({
-                typeRoom: "friend",
-                isDeleted: false,
-                participantsHash: hash,
-                users: [
-                    {
-                        userId: sender._id,
-                        nickname: sender.displayName,
-                        role: "owner"
-                    },
-                    {
-                        userId: receiver._id,
-                        nickname: receiver.displayName,
-                        role: "owner"
-                    }
-                ]
+        const existingRoom = await RoomChat.findOne({
+            participantsHash: hash,
+            isDeleted: false
+        });
+        if (existingRoom) {
+            return res.status(200).json({
+                message: "Phòng đã tồn tại",
+                room: existingRoom
             });
-            return res.status(201).json(room);
-        } catch (error) {
-            if (error.code === 11000) {
-                const existingRoom = await RoomChat.findOne({
-                    participantsHash: hash,
-                    isDeleted: false
-                });
-                return res.status(200).json({
-                    message: "Phòng đã có",
-                    existingRoom: existingRoom
-                });
-            }
-
-            throw error;
         }
+        const receiver = await User.findById(receiverId).select("displayName");
+        const sender = await User.findById(userId).select("displayName");
+        const room = await RoomChat.create({
+            typeRoom: "friend",
+            isDeleted: false,
+            participantsHash: hash,
+            users: [
+                {
+                    userId: sender._id,
+                    nickname: sender.displayName,
+                    role: "owner"
+                },
+                {
+                    userId: receiver._id,
+                    nickname: receiver.displayName,
+                    role: "owner"
+                }
+            ]
+        });
+        return res.status(201).json(room);
     } catch (error) {
+        if (error.code === 11000) {
+            const existingRoom = await RoomChat.findOne({
+                participantsHash: hash,
+                isDeleted: false
+            });
+            return res.status(200).json({
+                message: "Phòng đã có",
+                existingRoom: existingRoom
+            });
+        }
         return res.status(500).json({ message: error.message });
-
     }
-
-
 };
 export const getRoomChat = async (req, res) => {
     try {
