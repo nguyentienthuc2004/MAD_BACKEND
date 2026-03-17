818f9dc1903173f7c220c76d056ba5517c736310
Ryu <nguyenductruong200604@gmail.com>
2026-03-18 00:27:02 +0700

    Thêm api cho trạng thái tin nhắn

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getRoomChat, postRoomChat, getMessage, sendMessage, editNickname, createGroup, editRoom, getMember } from "../controllers/chat.controller.js";
+import { getRoomChat, postRoomChat, getMessage, sendMessage, editNickname, createGroup, seenMessage, editRoom, getMember } from "../controllers/chat.controller.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.get("/rooms", authenticate, getRoomChat);
@@ -10,4 +10,5 @@ router.patch("/rooms/:roomId/users/:userId/nickname", authenticate, editNickname
 router.post("/groups", authenticate, createGroup);
 router.patch("/rooms/:roomId", authenticate, editRoom);
 router.get("/groups/:roomId/member", authenticate, getMember);
+router.patch("/:roomId/seen", authenticate, seenMessage);
 export default router;
