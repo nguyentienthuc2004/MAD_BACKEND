a460242de8c15c480de2d5de722c6cafda3b80ef
Ryu <nguyenductruong200604@gmail.com>
2026-04-01 11:26:47 +0700

    Fix post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -137,10 +137,6 @@ export const createPost = async (req, res) => {
       moderationStatus: imageModerationResult.isSensitive ? "flagged" : "clean",
       moderationFlags: imageModerationResult.flags,
     });
-
-    // Ghi nhận hoạt động xem (view) khi tạo bài viết
-    await createViewActivity(user.userId, newPost._id);
-
     return res.status(201).json({
       success: true,
       message: "Post created successfully",
